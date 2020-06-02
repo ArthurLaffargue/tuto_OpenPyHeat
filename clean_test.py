f35bd04c8bc1abae740c9628d8b08f8d6241556b
PythonIngenieur <python.ingenieur@gmail.com>
2020-06-02 09:51:48 +0200

    Ajout du clean des tests

---
+++ clean_test.py
@@ -0,0 +1,37 @@
+# coding: utf-8
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from OpenPyHeat_2020.OpenPyHeat_Reader import allclean
+
+
+
+allclean('Example/triCouche0D')
+
+
+allclean('Example/triCouche0D_air')
+
+
+allclean('Example/monocouche_source_constante')
+
+
+allclean('Example/tri_couche')
+
+
+allclean('Example/tri_couche_steady')
+
+
+allclean('Example/cylindric_1')
+
+
+allclean('Example/cylindric_2')
+
+
+allclean('Example/cylindric_3')
+
+
+allclean('Example/monocouche_transient')
+
+
+allclean('Example/paraffinExchanger')
