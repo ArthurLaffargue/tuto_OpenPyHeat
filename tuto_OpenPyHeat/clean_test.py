4ccc632e4387e00d4c34c1b7b31638158361995e
Arthur LAFFARGUE <python.ingenieur@gmail.com>
2020-10-09 17:21:27 +0200

    Add files via upload
    Ajout echangeur eau air

---
+++ tuto_OpenPyHeat/clean_test.py
@@ -0,0 +1,43 @@
+# coding: utf-8
+
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+sys.path.append("..")
+from OpenPyHeat_2020.OpenPyHeat_Reader import allclean
+
+
+
+allclean('Example/triCouche0D')
+
+
+allclean('Example/triCouche0D_air')
+
+
+allclean('Example/monocouche_source_constante')
+
+
+allclean('Example/tri_couche')
+
+
+allclean('Example/tri_couche_steady')
+
+
+allclean('Example/cylindric_1')
+
+
+allclean('Example/cylindric_2')
+
+
+allclean('Example/cylindric_3')
+
+
+allclean('Example/monocouche_transient')
+
+
+allclean('Example/paraffinExchanger')
+
+
+allclean('Example/echangeur_air_eau')
+
